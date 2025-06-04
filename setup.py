1d8881039d51d97a6d5ae9322472d004a73a9a31
hardikkanekar <hardik.kanekar8109@gmail.com>
2025-06-04 13:03:26 +0530

    updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Hardik',
+    author_email= 'hardik.kanekar8109@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
